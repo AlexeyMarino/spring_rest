42ab79d9df44a01ff47ae8fbd1598f62950e0df6
Alexey Panteleev <iMar812@yandex.ru>
2021-09-08 19:36:25 +0400

    exception handling is implemented

--- src/main/java/ru/alexeymarino/spring/rest/controller/MyController.java
+++ src/main/java/ru/alexeymarino/spring/rest/controller/MyController.java
@@ -1,11 +1,12 @@
 package ru.alexeymarino.spring.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.alexeymarino.spring.rest.entity.Employee;
+import ru.alexeymarino.spring.rest.exception_handling.EmployeeIncorrectData;
+import ru.alexeymarino.spring.rest.exception_handling.NoSuchEmployeeException;
 import ru.alexeymarino.spring.rest.service.EmployeeService;
 
 import java.util.List;
@@ -25,6 +26,24 @@ public class MyController {
 
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
-        return employeeService.getEmployee(id);
+        Employee employee = employeeService.getEmployee(id);
+        if(employee == null) {
+            throw new NoSuchEmployeeException("Работника с ID = " + id + " в базеданных не найдено.");
+        }
+        return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException noSuchEmployeeException) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(noSuchEmployeeException.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST );
     }
 }
---
+++ src/main/java/ru/alexeymarino/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,10 @@
+package ru.alexeymarino.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+
+
+
+}
